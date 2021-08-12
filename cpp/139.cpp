8224df69eb3be054e84e4b7a124fde13e4a9c3ab
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2021-08-12 20:18:37 +0800

    AC 139 with cpp

---
+++ cpp/139.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Format Style: LLVM
+
+class Solution {
+public:
+  bool wordBreak(string s, vector<string> &wordDict) {
+    map<string, bool> memo = {};
+    return dp(s, wordDict, memo);
+  }
+
+  bool dp(string s, vector<string> &wordDict, map<string, bool> &memo) {
+    // find in memo
+    if (memo.find(s) != memo.end())
+      return memo.at(s);
+
+    // s equal to ""
+    if (!s.compare(""))
+      return true;
+
+    for (int i = 0; i < wordDict.size(); i++) {
+      // check if s start with wordDict[i]
+      if (s.rfind(wordDict[i], 0) == 0) {
+        string reminder = s;
+        reminder.replace(0, wordDict[i].length(), "");
+        bool reminderResult = dp(reminder, wordDict, memo);
+        if (reminderResult == true) {
+          memo.insert(make_pair(s, true));
+          return memo.at(s);
+        } else {
+          continue;
+        }
+      }
+    }
+
+    memo.insert(make_pair(s, false));
+    return memo.at(s);
+  }
+};
+
+int main() {
+  Solution solution;
+  vector<string> wordDict = {"leet", "code"};
+  cout << solution.wordBreak("leetcode", wordDict);
+  wordDict = {"apple", "pen"};
+  cout << solution.wordBreak("applepenapple", wordDict);
+  wordDict = {"cats", "dog", "sand", "and", "cat"};
+  cout << solution.wordBreak("acatsandog", wordDict);
+  wordDict = {"cats", "dog", "sand", "and", "cat"};
+  cout << solution.wordBreak("acatsandandcatdogcatssanddog", wordDict);
+}
