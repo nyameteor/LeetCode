3747104200a97c474b9a86efb71975cfb41eeabc
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2021-08-13 14:35:40 +0800

    AC 140 with java

---
+++ java/src/140.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    HashMap<String, List<String>> memo = new HashMap<>();
+    return dp(s, wordDict, memo);
+  }
+
+  private List<String> dp(String s, List<String> wordDict, HashMap<String, List<String>> memo) {
+    if (memo.containsKey(s))
+      return memo.get(s);
+    if (s.equals(""))
+      return List.of("");
+
+    List<String> ans = new ArrayList<>();
+
+    for (String word : wordDict) {
+      if (s.startsWith(word)) {
+        String reminder = s.substring(word.length());
+        List<String> reminderResults = dp(reminder, wordDict, memo);
+
+        if (!reminderResults.equals(List.of())) {
+          List<String> sResults = reminderResults.stream().map(e -> resultAdd(e, word)).collect(Collectors.toList());
+          ans.addAll(sResults);
+        }
+      } else {
+        continue;
+      }
+    }
+
+    memo.put(s, ans);
+    return memo.get(s);
+  }
+
+  private String resultAdd(String e, String word) {
+    // edge case
+    if (e == "") {
+      return word + e;
+    } else {
+      return word + " " + e;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+
+    // [cat sand dog, cats and dog]
+    System.out.println(solution.wordBreak("catsanddog", List.of("cat", "cats", "and", "sand", "dog")));
+
+    // [pine apple pen apple, pine applepen apple, pineapple pen apple]
+    System.out
+        .println(solution.wordBreak("pineapplepenapple", List.of("apple", "pen", "applepen", "pine", "pineapple")));
+
+    // []
+    System.out.println(solution.wordBreak("catsandog", List.of("cat", "cats", "and", "sand", "dog")));
+  }
+}
