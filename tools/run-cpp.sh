960948c8f63650b0edcc25cea216d472846be102
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2021-08-13 14:38:54 +0800

    chore: little update on doc and tool

--- tools/run-cpp.sh
+++ tools/run-cpp.sh
@@ -12,7 +12,7 @@ readonly OUT_FILE="solution.out"
 main() {
   local src_file="${1}"
 
-  g++ -std=c++14 "${src_file}" --output "${OUT_DIR}/${OUT_FILE}" && cd "${OUT_DIR}" && "${OUT_DIR}/${OUT_FILE}"
+  g++ -g -std=c++14 "${src_file}" --output "${OUT_DIR}/${OUT_FILE}" && cd "${OUT_DIR}" && "${OUT_DIR}/${OUT_FILE}"
 }
 
 main "${1}"
