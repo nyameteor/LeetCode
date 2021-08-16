92ffc332380b27d8f739238ceabe760183ccdad6
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2021-08-16 17:45:29 +0800

    AC 16 with cpp

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 | 139 | [Word Break](https://leetcode.com/problems/word-break/) | [C++](code/cpp/139.cpp),[Java](code/java/139.java),[JS](code/js/139.js),[Python](code/py3/139.py) | Medium | [📃](docs/139.%20Word%20Break.md) |
 | 140 | [Word Break II](https://leetcode.com/problems/word-break-ii/) | [C++](code/cpp/140.cpp),[Java](code/java/140.java) | Hard | [📃](docs/140.%20Word%20Break%20II.md) |
 | 15 | [3Sum](https://leetcode.com/problems/3sum/) | [C++](code/cpp/15.cpp) | Medium | [📃](docs/15.%203Sum.md) |
+| 16 | [3Sum Closest](https://leetcode.com/problems/3sum-closest/) | [C++](code/cpp/16.cpp) | Medium | [📃](docs/16.%203Sum%20Closest.md) |
 | 167 |  | [C++](code/cpp/167.cpp) |  |  |
 | 26 | [Remove Duplicates from Sorted Array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/) | [C++](code/cpp/26.cpp),[Python](code/py3/26.py) | Easy | [📃](docs/26.%20Remove%20Duplicates%20from%20Sorted%20Array.md) |
 | 448 | [Find All Numbers Disappeared in an Array](https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/) | [C++](code/cpp/448.cpp) | Easy | [📃](docs/448.%20Find%20All%20Numbers%20Disappeared%20in%20an%20Array.md) |
---
+++ code/cpp/16.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int threeSumClosest(vector<int> &nums, int target) {
+    // edge case
+    if (nums.size() == 3) {
+      return nums[0] + nums[1] + nums[2];
+    }
+
+    // sort beforce using two pointers
+    sort(nums.begin(), nums.end());
+
+    // big enough initial value
+    int cloest_sum = 999999;
+
+    for (int i = 0; i < nums.size() - 2; i++) {
+      // left pointer
+      int l = i + 1;
+      // right pointer
+      int r = nums.size() - 1;
+
+      while (l < r) {
+        int sum = nums[i] + nums[l] + nums[r];
+        int diff = sum - target;
+        int distance = abs(sum - target);
+        int cloest_distance = abs(cloest_sum - target);
+
+        if (diff < 0) {
+          // update ans
+          if (cloest_distance > distance)
+            cloest_sum = sum;
+          l++;
+        } else if (diff > 0) {
+          if (cloest_distance > distance)
+            cloest_sum = sum;
+          r--;
+        } else if (diff == 0) {
+          return target;
+        }
+      }
+    }
+    return cloest_sum;
+  }
+};
+
+int main() {
+  Solution solution;
+
+  // 3 <= nums.length <= 1000
+  // -1000 <= nums[i] <= 1000
+  // -104 <= target <= 104
+  vector<int> nums;
+
+  // 2
+  nums = {-1, 2, 1, -4};
+  cout << solution.threeSumClosest(nums, 1) << endl;
+
+  // 0
+  nums = {0, 2, 1, -3};
+  cout << solution.threeSumClosest(nums, 1) << endl;
+
+  // 3
+  nums = {1, 1, 1, 1};
+  cout << solution.threeSumClosest(nums, -100) << endl;
+
+  // 0
+  nums = {0, 0, 0};
+  cout << solution.threeSumClosest(nums, 1) << endl;
+}
