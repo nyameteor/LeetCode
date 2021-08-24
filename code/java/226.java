ad468a41a0137beef148b96df6a6689999d8b6a7
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2021-08-24 15:38:33 +0800

    AC 226 with java

---
+++ code/java/226.java
@@ -0,0 +1,37 @@
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode() {
+  }
+
+  TreeNode(int val) {
+    this.val = val;
+  }
+
+  TreeNode(int val, TreeNode left, TreeNode right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Solution {
+  public TreeNode invertTree(TreeNode root) {
+    if (root == null)
+      return null;
+
+    swap(root);
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+  }
+
+  private void swap(TreeNode root) {
+    TreeNode tmp = root.left;
+    root.left = root.right;
+    root.right = tmp;
+  }
+}
