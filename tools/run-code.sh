09ec4aa840a4a3a40e56009b2552971042be3abc
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2021-08-27 00:49:10 +0800

    style: update shell indentation

--- tools/run-code.sh
+++ tools/run-code.sh
@@ -11,38 +11,38 @@ readonly JAVA_OUT="Solution"
 readonly CPP_OUT="solution.out"
 
 run_cpp() {
-  local src_file="${1}"
+    local src_file="${1}"
 
-  g++ -g -std=c++14 "${src_file}" --output "${OUT_DIR}/${CPP_OUT}" &&
-    "${OUT_DIR}/${CPP_OUT}"
+    g++ -g -std=c++14 "${src_file}" --output "${OUT_DIR}/${CPP_OUT}" &&
+        "${OUT_DIR}/${CPP_OUT}"
 }
 
 run_java() {
-  local src_file="${1}"
+    local src_file="${1}"
 
-  javac -d "${OUT_DIR}" "${src_file}" &&
-    cd "${OUT_DIR}" &&
-    java -ea "${JAVA_OUT}"
+    javac -d "${OUT_DIR}" "${src_file}" &&
+        cd "${OUT_DIR}" &&
+        java -ea "${JAVA_OUT}"
 }
 
 main() {
-  local src_file="${1}"
+    local src_file="${1}"
 
-  # Refer: https://stackoverflow.com/a/965069
-  local filename=$(basename -- "${src_file}")
-  local extension="${filename##*.}"
+    # Refer: https://stackoverflow.com/a/965069
+    local filename=$(basename -- "${src_file}")
+    local extension="${filename##*.}"
 
-  case "${extension}" in
-  cpp)
-    run_cpp "${src_file}"
-    ;;
-  java)
-    run_java "${src_file}"
-    ;;
-  *)
-    echo "Not a valid source file."
-    ;;
-  esac
+    case "${extension}" in
+    cpp)
+        run_cpp "${src_file}"
+        ;;
+    java)
+        run_java "${src_file}"
+        ;;
+    *)
+        echo "Not a valid source file."
+        ;;
+    esac
 }
 
 main "${1}"
