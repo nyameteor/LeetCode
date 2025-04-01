e10d5ad13ff7f8c46822e524e1ec204cd2e48da0
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2025-04-01 19:52:16 +0800

    Update 77

--- problems/combinations/answer.go
+++ problems/combinations/answer.go
@@ -2,27 +2,85 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"slices"
+	"sort"
 )
 
 func combine(n int, k int) [][]int {
+	return topDownBacktrack(n, k)
+}
+
+func topDownBacktrack(n int, k int) [][]int {
 	res := make([][]int, 0)
 	path := make([]int, 0, k)
-	dfs(n, k, 1, &path, &res)
+
+	var dfs func(start int)
+	dfs = func(start int) {
+		if len(path) == k {
+			res = append(res, slices.Clone(path))
+			return
+		}
+
+		for i := start; i <= n; i++ {
+			path = append(path, i)
+			dfs(i + 1)
+			path = path[:len(path)-1]
+		}
+	}
+
+	dfs(1)
 	return res
 }
 
-func dfs(n int, k int, start int, path *[]int, res *[][]int) {
-	if len(*path) == k {
-		*res = append(*res, slices.Clone(*path))
-		return
+func topDown(n int, k int) [][]int {
+	res := make([][]int, 0)
+	path := make([]int, 0)
+
+	var dfs func(start int, path []int)
+	dfs = func(start int, path []int) {
+		if len(path) == k {
+			res = append(res, slices.Clone(path))
+			return
+		}
+
+		for i := start; i <= n; i++ {
+			dfs(i+1, append(path, i))
+		}
 	}
 
-	for i := start; i <= n; i++ {
-		*path = append(*path, i)
-		dfs(n, k, i+1, path, res)
-		*path = (*path)[:len(*path)-1]
+	dfs(1, path)
+	return res
+}
+
+func combinationsEqual(c1, c2 [][]int) bool {
+	if len(c1) != len(c2) {
+		return false
 	}
+	for i := range c1 {
+		if len(c1[i]) != len(c2[i]) {
+			return false
+		}
+	}
+
+	for i := range c1 {
+		sort.Ints(c1[i])
+		sort.Ints(c2[i])
+	}
+
+	slicesCompare := func(s1, s2 []int) bool {
+		for i := range s1 {
+			if s1[i] != s2[i] {
+				return s1[i] < s2[i]
+			}
+		}
+		return false
+	}
+
+	sort.Slice(c1, func(i, j int) bool { return slicesCompare(c1[i], c1[j]) })
+	sort.Slice(c2, func(i, j int) bool { return slicesCompare(c2[i], c2[j]) })
+
+	return reflect.DeepEqual(c1, c2)
 }
 
 func main() {
@@ -58,12 +116,31 @@ func main() {
 		},
 	}
 
-	for _, tc := range testCases {
-		actual := combine(tc.input.n, tc.input.k)
-		if !slices.EqualFunc(actual, tc.expected, func(x, y []int) bool {
-			return slices.Equal(x, y)
-		}) {
-			fmt.Printf("combine(%v) = %v, expected = %v\n", tc.input, actual, tc.expected)
+	solutions := []struct {
+		name string
+		fn   func(n int, k int) [][]int
+	}{
+		{
+			name: "Default Solution",
+			fn:   combine,
+		},
+		{
+			name: "Top-down",
+			fn:   topDown,
+		},
+		{
+			name: "Top-down with backtracking",
+			fn:   topDownBacktrack,
+		},
+	}
+
+	for _, solution := range solutions {
+		for _, tc := range testCases {
+			actual := solution.fn(tc.input.n, tc.input.k)
+			if !combinationsEqual(actual, tc.expected) {
+				fmt.Printf("[FAILED] Name: %s | Input: %v | Output: %v | Expected: %v\n",
+					solution.name, tc.input, actual, tc.expected)
+			}
 		}
 	}
 }
