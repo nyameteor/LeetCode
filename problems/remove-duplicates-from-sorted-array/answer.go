4859466a1e37ac0c325aa426eee510095ada70e9
Nya Meteor <26038696+nyameteor@users.noreply.github.com>
2025-04-16 13:55:08 +0800

    Update 26

--- problems/remove-duplicates-from-sorted-array/answer.go
+++ problems/remove-duplicates-from-sorted-array/answer.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
 	l := 0
 	r := 1
 	for r < len(nums) {
@@ -16,13 +23,26 @@ func removeDuplicates(nums []int) int {
 }
 
 func main() {
-	var nums []int
+	testCases := []struct {
+		input    []int
+		expected []int
+	}{
+		{
+			input:    []int{1, 1, 2},
+			expected: []int{1, 2},
+		},
+		{
+			input:    []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4},
+			expected: []int{0, 1, 2, 3, 4},
+		},
+	}
 
-	nums = []int{1, 1, 2}
-	fmt.Println(removeDuplicates(nums))
-	fmt.Println(nums)
-
-	nums = []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	fmt.Println(removeDuplicates(nums))
-	fmt.Println(nums)
+	for _, tc := range testCases {
+		input := slices.Clone(tc.input)
+		k := removeDuplicates(input)
+		actual := input[:k]
+		if !slices.Equal(actual, tc.expected) {
+			fmt.Printf("removeDuplicates(%v) = %v, expected = %v\n", tc.input, actual, tc.expected)
+		}
+	}
 }
